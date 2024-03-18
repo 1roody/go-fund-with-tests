0c73321283810e50cb51a44b0d5aa1465214b331
rodrigoflstk <jrodrigo0592@gmail.com>
2024-03-18 10:40:41 -0300

    arrays and slices

---
+++ 04-arrays-slices/arrays-slices.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Sum(numbers []int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println(Sum([]int{1, 2, 3, 4, 5}))
+}
