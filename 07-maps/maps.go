2dd8b47344c9d15061d2c75ae9d18188649b0431
rodrigoflstk <jrodrigo0592@gmail.com>
2024-03-24 06:42:37 -0300

    maps crud

--- 07-maps/maps.go
+++ 07-maps/maps.go
@@ -1,17 +1,58 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
+type DictionaryErr string
 
-var errNotFound = errors.New("could not find the word you're looking for")
+var (
+	errNotFound         = DictionaryErr("could not find the word you're looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
-	result, ok := d[word]
+	definition, ok := d[word]
 
 	if !ok {
-		return "", errNotFound
+		return " ", errNotFound
 	}
 
-	return result, nil
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case errNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case errNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = newDefinition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
